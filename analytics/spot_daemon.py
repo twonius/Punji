5f00e1056c214919bdbe4b71c2136fb0900b1302
twonius <antonemaes@gmail.com>
2021-03-16 20:14:15 -0700

    fixed password

--- analytics/spot_daemon.py
+++ analytics/spot_daemon.py
@@ -1,9 +1,13 @@
 from pymongo import MongoClient
 # pprint library is used to make the output look more pretty
-from pprint import pprint
+from prettyprinter import pprint
 # connect to MongoDB, change the << MONGODB URL >> to reflect your own connection string
-client = MongoClient('mongodb+srv://user:yqpAl6Q4oHLliI00@beta.d501j.mongodb.net/spot?retryWrites=true&w=majority')
-db=client.admin
+client = MongoClient('mongodb+srv://user:cqXSE7EzLgh2LC9@beta.d501j.mongodb.net/spot?retryWrites=true&w=majority')
+db=client.db
 # Issue the serverStatus command and print the results
-serverStatusResult=db.command("serverStatus")
-pprint(serverStatusResult)
+# serverStatusResult=db.command("serverStatus")
+# pprint(serverStatusResult)
+
+sensordatas=db.sensordatas
+
+pprint(sensordatas)
